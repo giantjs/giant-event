af17e7c8cb026fb70929f8477f8653648cb4c1e3
Dan Stocker <dan.stocker@mangahigh.com>
2015-09-24 15:05:45 +0100

    restores $utils namespace

--- src/namespace.js
+++ src/namespace.js
@@ -9,7 +9,9 @@ var $assertion = $assertion || require('giant-assertion');
 /** @namespace */
 var $oop = $oop || require('giant-oop');
 
+/** @namespace */
+var $utils = $utils || require('giant-utils');
+
 if (typeof require === 'function') {
     require('giant-data');
-    require('giant-utils');
 }
